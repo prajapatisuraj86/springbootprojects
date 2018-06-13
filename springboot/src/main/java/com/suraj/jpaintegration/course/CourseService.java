8d596911ce4238208efb0d62ba13d78c80160460
Suraj Prajapati <suraj.prajapati@elitecore.co.in>
2018-06-13 10:48:13 +0530

    Log4j in Courses

--- springboot/src/main/java/com/suraj/jpaintegration/course/CourseService.java
+++ springboot/src/main/java/com/suraj/jpaintegration/course/CourseService.java
@@ -28,16 +28,24 @@ public class CourseService {
 	TopicRepository topicRepository;
 	
 	public List<Course> getAllCourses(String topicId) {
-		return courseRepository.findByTopicId(topicId);
+		String strMethodName = "getAllCourses";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started with :"+topicId));
+		List<Course> lstCourses = courseRepository.findByTopicId(topicId);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended with :"+lstCourses));
+		return lstCourses;
 	}
 	
 	public Course getCourse(String id) {
-		return courseRepository.findOne(id);
+		String strMethodName = "getCourse";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started with Course Id :"+id));
+		Course course = courseRepository.findOne(id);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended with :"+course));
+		return course;
 	}
 	
 	public void addCourse(Course course, String topicId) {
 		String strMethodName = "addCourse";
-		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started"));
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started with course :"+course+ " and Topic Id :"+topicId));
 		course.setTopic(topicRepository.findOne(topicId));
 		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Course is :"+course));
 		courseRepository.save(course);
@@ -45,12 +53,18 @@ public class CourseService {
 	}
 	
 	public void updateCourse(Course course, String topicId) {
+		String strMethodName = "updateCourse";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started with course :"+course+ " and Topic Id :"+topicId));
 		course.setTopic(topicRepository.findOne(topicId));
 		courseRepository.save(course);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
 	}
 	
 	public void deleteCourse(String id) {
+		String strMethodName = "deleteCourse";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started with Course Id :"+id));
 		courseRepository.delete(id);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
 	}
 
 }
