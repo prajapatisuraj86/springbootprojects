3460fb62715249ae330e8ffef7441dfaa8705010
Suraj Prajapati <suraj.prajapati@elitecore.co.in>
2018-06-14 12:26:30 +0530

    Added Loggers in Controllers

--- springboot/src/main/java/com/suraj/jpaintegration/course/CourseController.java
+++ springboot/src/main/java/com/suraj/jpaintegration/course/CourseController.java
@@ -2,6 +2,8 @@ package com.suraj.jpaintegration.course;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,36 +11,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.suraj.jpaintegration.util.CommonUtility;
+
 @RestController
 public class CourseController {
+	
+	private static final String CLASSNAME = "CourseController";
+	private static final Logger logger = LogManager.getLogger(CourseController.class.getSimpleName());
 
 	@Autowired
 	private CourseService courseService;
 	
 	@RequestMapping("/topics/{topicId}/courses")
 	public List<Course> getAllCourses(@PathVariable("topicId") String topicId) {
-		return courseService.getAllCourses(topicId);
+		String strMethodName = "getAllCourses";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started"));
+		List<Course> lstCourses = courseService.getAllCourses(topicId);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
+		return lstCourses;
 	}
 	
 	@RequestMapping("/topics/{topicId}/courses/{id}")
 	public Course getCourse(@PathVariable("topicId") String topicId, @PathVariable("id") String id) {
-		return courseService.getCourse(id);
+		String strMethodName = "getCourse";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started"));
+		Course course = courseService.getCourse(id);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
+		return course;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, value = "/topics/{topicId}/courses")
 	public void addCourse(@RequestBody Course course, @PathVariable("topicId") String topicId) {
+		String strMethodName = "addCourse";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started"));
 		courseService.addCourse(course, topicId);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
 	}
 	
 	
 	@RequestMapping(method=RequestMethod.PUT, value = "/topics/{topicId}/courses")
 	public void updateCourse(@RequestBody Course course, @PathVariable("topicId") String topicId) {
+		String strMethodName = "updateCourse";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started"));
 		courseService.updateCourse(course, topicId);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
 	}
 	
 	@RequestMapping(method=RequestMethod.DELETE, value = "topics/{topicId}/courses/{id}")
 	public void deleteCourse(@PathVariable("topicId") String topicId, @PathVariable("id") String id) {
+		String strMethodName = "deleteCourse";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started"));
 		courseService.deleteCourse(id);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
 	}
 	
 }
--- springboot/src/main/java/com/suraj/jpaintegration/topic/TopicController.java
+++ springboot/src/main/java/com/suraj/jpaintegration/topic/TopicController.java
@@ -2,6 +2,8 @@ package com.suraj.jpaintegration.topic;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,36 +11,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.suraj.jpaintegration.course.CourseController;
+import com.suraj.jpaintegration.util.CommonUtility;
+
 @RestController
 public class TopicController {
+	
+	private static final String CLASSNAME = "CourseController";
+	private static final Logger logger = LogManager.getLogger(CourseController.class.getSimpleName());
 
 	@Autowired
 	private TopicService topicService;
 	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopic() {
-		return topicService.getAllTopic();
+		String strMethodName = "getAllTopic";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started"));
+		List<Topic> lstTopics = topicService.getAllTopic();
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
+		return lstTopics;
 	}
 	
 	@RequestMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable String id) {
-		return topicService.getTopic(id);
+		String strMethodName = "getTopic";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started"));
+		Topic topic = topicService.getTopic(id);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
+		return topic;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, value = "/topics")
 	public void addTopic(@RequestBody Topic topic) {
+		String strMethodName = "addTopic";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started"));
 		topicService.addTopic(topic);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
 	}
 	
 	
 	@RequestMapping(method=RequestMethod.PUT, value = "/topics/{id}")
 	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		String strMethodName = "updateTopic";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started"));
 		topicService.updateTopic(topic, id);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
 	}
 	
 	@RequestMapping(method=RequestMethod.DELETE, value = "/topics/{id}")
-	public void updateTopic(@PathVariable String id) {
+	public void deleteTopic(@PathVariable String id) {
+		String strMethodName = "deleteTopic";
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Started"));
 		topicService.deleteTopic(id);
+		logger.info(CommonUtility.getSampleLogger(CLASSNAME, strMethodName, "Ended"));
 	}
 	
 }
