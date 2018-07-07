e0ed4475aff1fbf7e9567e8477e6ab4b1cc59e7f
Suraj Prajapati <prajapatisuraj86@gmail.com>
2018-07-07 14:07:22 +0530

    Warnings

--- springboot/src/main/java/com/suraj/jpaintegration/topic/TopicController.java
+++ springboot/src/main/java/com/suraj/jpaintegration/topic/TopicController.java
@@ -1,7 +1,5 @@
 package com.suraj.jpaintegration.topic;
 
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
