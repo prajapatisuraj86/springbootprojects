17be236617778cbfc9e1369ddce83d36c88089f7
Suraj Prajapati <prajapatisuraj86@gmail.com>
2018-07-07 13:38:33 +0530

    Constant added

---
+++ springboot/src/main/java/com/suraj/jpaintegration/constant/BaseConstant.java
@@ -0,0 +1,8 @@
+package com.suraj.jpaintegration.constant;
+
+public class BaseConstant {
+	
+	public static final String GET_TOPIC_ERROR = "Error occoured while getting Topics";
+	public static final String GET_TOPIC_SUCCESS = "Topics are fetched successfully";
+
+}
